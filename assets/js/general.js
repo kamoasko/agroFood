aa17dfc2a3bab44192f9ebc3a1dd384024a89c5b
kamoasko <kamdesigning@gmail.com>
2024-03-02 14:14:16 +0400

    header done

--- assets/js/general.js
+++ assets/js/general.js
@@ -6,21 +6,49 @@ let logo = document.querySelector(".header__logo");
 let navbarMenu = document.querySelector(".nav_menu_search");
 let navbarRight = document.querySelector(".navbar__right");
 let closeSearchForm = document.querySelector(".search__close");
+let hamburgerIcon = document.querySelector(".hamburger__icon");
 
-closeSearchForm.addEventListener("click", () => {
+let responsiveWidth = window.innerWidth;
+let header = document.querySelector("header");
+
+closeSearchForm.addEventListener("click", (event) => {
+  event.stopPropagation();
+
   searchForm.classList.remove("isActive");
   logo.classList.remove("isActive");
-  navbarMenu.classList.remove("isActive");
-  navbarRight.classList.remove("isActive");
+
+  if (responsiveWidth > 992) {
+    navbarMenu.classList.remove("isActive");
+    navbarRight.classList.remove("isActive");
+  } else {
+    // Check if menu is hidden before adding class
+    if (!navbarMenu.classList.contains("isActive")) {
+      navbarMenu.classList.add("isActive");
+    }
+    navbarRight.classList.remove("isActive");
+    header.style.height = null; // Remove height and alignItems styles
+    header.style.alignItems = null;
+  }
 });
 
 function openActives() {
   openSearch.addEventListener("click", () => {
-    searchForm.classList.add("isActive");
-    logo.classList.add("isActive");
-    navbarRight.classList.add("isActive");
-    if (!navbarMenu.classList.contains("isActive")) {
+    if (responsiveWidth <= 992) {
+      searchForm.classList.add("isActive");
+      logo.classList.add("isActive");
+      navbarRight.classList.add("isActive");
+
+      // Only hide menu if it wasn't already open
+      if (navbarMenu.classList.contains("isActive")) {
+        navbarMenu.classList.remove("isActive");
+        header.style.height = "100vh"; // Remove height and alignItems styles
+        header.style.alignItems = "stretch";
+      }
+    } else {
+      navbarRight.classList.add("isActive");
+      searchForm.classList.add("isActive");
       navbarMenu.classList.add("isActive");
+      logo.classList.add("isActive");
     }
   });
 }
@@ -33,5 +61,21 @@ $(document).ready(function () {
   $(".hamburger__icon").click(function () {
     $(this).toggleClass("open");
     $(".nav_menu").toggleClass("isActive");
+  });
+});
+
+// mobile submenu dropdown
+
+let openSubBtns = document.querySelectorAll(".caret");
+let subMenus = document.querySelectorAll(".sub_menu");
+
+openSubBtns.forEach((a) => {
+  console.log(a);
+});
+
+openSubBtns.forEach((openSubBtn) => {
+  openSubBtn.addEventListener("click", (e) => {
+    e.currentTarget.classList.toggle("opened");
+    e.currentTarget.nextSibling.classList.toggle("opened");
   });
 });
