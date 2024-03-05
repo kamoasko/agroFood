686a496582f455e478f2f1a0325e0df76e2cd6c8
kamal-aliyev <myangels.n1@gmail.com>
2024-03-05 15:06:00 +0400

    fixed some styles in about page

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,3 +1,4 @@
+let currentPage = "who_us";
 
 const aboutImage = document.querySelector('.about__headSection--image img');
 
@@ -25,41 +26,26 @@ function changeImage () {
     cooperationLink.classList.remove("active");
     responsibilityLink.classList.remove("active");
 
-    switch (currentPath) {
-        case "/about":
-        case "/az/about":
-        case "/en/about":
-        case "/ru/about":
+    switch (currentPage) {
+        case "who_us":
             aboutImage.src = "../assets/img/about/whoWe.png";           // nav'in yanindaki sekil 
             whoWe.classList.add("active");
             aboutLink.classList.add("active");
         break;
 
-        case "/strategies":        // FOR LOCALE (DELETE THIS)
-        case "/about/strategies":
-        case "/az/about/strategies":
-        case "/en/about/strategies":
-        case "/ru/about/strategies":
+        case "strategies":        // FOR SLUG 
             aboutImage.src = "../assets/img/about/strategies.png";          // nav'in yanindaki sekil 
             strategies.classList.add("active");
             strategiesLink.classList.add("active");
         break;
 
-        case "/cooperation":        // FOR LOCALE (DELETE THIS)
-        case "/about/cooperation":
-        case "/az/about/cooperation":
-        case "/ru/about/cooperation":
-        case "/en/about/cooperation":
+        case "for_corporation":        // FOR SLUG 
             aboutImage.src = "../assets/img/about/cooperation.png";         // nav'in yanindaki sekil 
             cooperation.classList.add("active")
             cooperationLink.classList.add("active");
         break;
 
-        case "/responsibility":        // FOR LOCALE (DELETE THIS)
-        case "/about/responsibility":
-        case "/az/about/responsibility":
-        case "/en/about/responsibility":
-        case "/ru/about/responsibility":
+        case "responsibilty":        // FOR SLUG 
             aboutImage.src = "../assets/img/about/resp.png";            // nav'in yanindaki sekil 
             responsibility.classList.add("active")
             responsibilityLink.classList.add("active");
@@ -67,23 +53,23 @@ function changeImage () {
     } 
 }
 
-// changeImage();   turn on this !!!
+changeImage();
 
 
 
 aboutLink.addEventListener('click', () => {
-    history.pushState({}, '', 'about');                     // about nav kecid linkleri
+    currentPage = "who_us";                         // about nav slug
     changeImage();
 })
 strategiesLink.addEventListener('click', () => {
-    history.pushState({}, '', 'strategies');                // about nav kecid linkleri
+    currentPage = "strategies"                 // about nav slug
     changeImage();
 })
 cooperationLink.addEventListener('click', () => {
-    history.pushState({}, '', 'cooperation');               // about nav kecid linkleri
+    currentPage = "for_corporation";               // about nav slug
     changeImage();
 })
 responsibilityLink.addEventListener('click', () => {
-    history.pushState({}, '', 'responsibility');                // about nav kecid linkleri
+    currentPage = "responsibilty";                // about nav slug
     changeImage();
 })
